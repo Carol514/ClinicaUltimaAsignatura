c15f0bae96fa6f6ca0273284e1e1dd5c0ef5aa60
Sophia Hinojosa <ti1901025@academica.utbb.edu.mx>
2025-11-08 17:17:52 -0600

    feat(ui): agregar layout base (app.blade), vista administrador y recursos estáticos (CSS e imagen)

--- Clinca/routes/web.php
+++ Clinca/routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('/administrador', 'administrador');
+Route::view('/administrar-roles', 'administrar_roles');   // luego creamos esta vista
+Route::view('/reportes', 'generador_reportes');           // luego creamos esta vista
+Route::view('/respaldo-bd', 'respaldo_bd');               // luego creamos esta vista

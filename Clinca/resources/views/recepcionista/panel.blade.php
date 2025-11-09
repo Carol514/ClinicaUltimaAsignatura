8ae60723be3f2bcf76be0807d44590070001acd7
Sophia Hinojosa <ti1901025@academica.utbb.edu.mx>
2025-11-08 21:28:32 -0600

    feat(ui): panel de Recepción y formulario de registro de paciente

---
+++ Clinca/resources/views/recepcionista/panel.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+@section('title', 'Panel de Recepción')
+
+@section('content')
+<main class="dashboard">
+  <h2>Panel de Recepción</h2>
+  <p class="muted">Selecciona una acción para comenzar.</p>
+
+  <div class="card-container" >
+    <a class="card" href="{{ route('recepcionista.registro') }}" style="text-decoration: none;">
+      Registrar paciente
+    </a>
+    <a class="card" href="#" style="text-decoration: none;">
+      Agendar cita
+    </a>
+    <a class="card" href="#" style="text-decoration: none;">
+      Ver agenda del día
+    </a>
+  </div>
+</main>
+@endsection
